f2fbdc2ccb7ff7a01c830b68b2ec0c1cf0fc888f
gabriel ruiz <gabriel_ruiz21@hotmail.com>
2020-12-01 12:50:37 -0500

    correcting the json file and adding some new dependencies

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -2,18 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 const { Post,Section,Quiz } = db;
-// This is a simple example for providing basic CRUD routes for
-// a resource/model. It provides the following:
-//    GET    /posts
-//    POST   /posts
-//    GET    /posts/:id
-//    PUT    /posts/:id
-//    DELETE /posts/:id 
-
-// There are other styles for creating these route handlers, we typically
-// explore other patterns to reduce code duplication.
-// TODO: Can you spot where we have some duplication below?
-
 
 router.get('/', (req,res) => {
   Post.findAll()
